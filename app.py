6c0770f2ec426641c839c296035df7d72d30b94a
shaoyu <shaoyu0209@tuta.io>
2021-08-01 06:32:44 +0800

    Change route, add CORS

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, request
 from rangescore.utils import percentiles
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/lemmatize/<string:lang>', methods=['POST'])
+@app.route('/analyze/<string:lang>', methods=['POST'])
 def lemmmatize(lang):
     if lang == 'ru' and request.method == 'POST':
         print(request.json)
