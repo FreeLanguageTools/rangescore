5bed16584763dc867de4ee371c5720469d6311eb
shaoyu <shaoyu0209@tuta.io>
2021-08-06 14:34:26 +0800

    Add healthcheck

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/healthcheck')
+def healthcheck():
+    return "Hello"
+
 @app.route('/analyze/<string:lang>', methods=['POST'])
 def lemmmatize(lang):
     if lang == 'ru' and request.method == 'POST':
